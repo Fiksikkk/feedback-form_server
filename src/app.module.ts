e6366cfee56f84023bf27c42aa4b1e1c3ed8ae8a
Fiksikkk <yakov.varenik@gmail.com>
2022-10-11 01:43:59 +0200

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,15 @@ import { SequelizeModule } from "@nestjs/sequelize";
 import { ConfigModule } from '@nestjs/config';
 import { User } from "./users/users.model";
 import { UsersModule } from './users/users.module';
+import { configService } from "./config/config.service";
+
 
 @Module({
     controllers: [],
     providers: [],
     imports: [
         ConfigModule.forRoot(),
-        SequelizeModule.forRoot({
-          url: process.env.DATABASE_URL,
-          type: 'postgres',
-          entities: ['dist/**/*.entity{.ts,.js}'],
-          synchronize: true, // This for development
-          autoLoadModels: true,
-        }),
+        SequelizeModule.forRoot(configService.getTypeOrmConfig()),
         UsersModule,
       ],
 })
