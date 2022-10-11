3e930af341c98a9b1a21b3ba45616f111c1999ea
Fiksikkk <yakov.varenik@gmail.com>
2022-10-11 02:04:30 +0200

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { UsersModule } from './users/users.module';
         SequelizeModule.forRoot({
           uri: process.env.DATABASE_URL,
           dialect: 'postgres',
+          synchronize: true,
           autoLoadModels: true,
         }),
         UsersModule,
